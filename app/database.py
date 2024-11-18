e103f3a822e1776c3deef62a2f829d28b71422b1
Anthony <anorozco@inteia.com.co>
2024-11-18 17:42:01 -0500

    db test

--- app/database.py
+++ app/database.py
@@ -1,10 +1,14 @@
+import os 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os 
-SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+print(os.getenv('DB_URL'))
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("DB_URL environment variable not set!")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
